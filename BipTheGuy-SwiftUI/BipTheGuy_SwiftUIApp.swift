5a3bb858eaf88345c2b5622e5a141438724228ca
MacModeler <memcbride@mac.com>
2024-06-24 12:28:23 -0400

    Initial Commit

---
+++ BipTheGuy-SwiftUI/BipTheGuy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftUIApp.swift
+//  BipTheGuy-SwiftUI
+//
+//  Created by Mark McBride on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
